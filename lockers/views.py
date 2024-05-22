3b1f2e9a3ce82713b797bbaffa541612152636af
yeomjeongwoo <yeomi0206@gmail.com>
2024-05-22 21:35:26 +0900

    make gift and exchange button

--- lockers/views.py
+++ lockers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .models import Locker
 
 def login_view(request):
     if request.method == 'POST':
@@ -9,16 +10,17 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')  # 로그인 후 index.html로 리디렉션
+            return redirect('index')
         else:
             return render(request, 'lockers/login.html', {'error': 'Invalid credentials'})
     return render(request, 'lockers/login.html')
 
 @login_required
 def index_view(request):
-    return render(request, 'lockers/index.html')
+    lockers = Locker.objects.all()
+    return render(request, 'lockers/index.html', {'lockers': lockers, 'username': request.user.username})
 
 @login_required
 def notifications_view(request):
-    # 알림 로직 추가
+    # 알림 로직을 여기에 추가할 수 있습니다.
     return render(request, 'lockers/notifications.html')
