c36483a65fbcb340d9f6651ff1711fb788258e56
yeomjeongwoo <yeomi0206@gmail.com>
2024-05-22 21:23:20 +0900

    lockerrrskrr

--- lockers/views.py
+++ lockers/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .models import Locker
 
 def login_view(request):
     if request.method == 'POST':
@@ -10,20 +9,14 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('floor1')
+            return redirect('index')  # 로그인 후 index.html로 리디렉션
         else:
             return render(request, 'lockers/login.html', {'error': 'Invalid credentials'})
     return render(request, 'lockers/login.html')
 
 @login_required
-def floor1_view(request):
-    lockers = Locker.objects.filter(floor=1)
-    return render(request, 'lockers/floor1.html', {'lockers': lockers})
-
-@login_required
-def floor2_view(request):
-    lockers = Locker.objects.filter(floor=2)
-    return render(request, 'lockers/floor2.html', {'lockers': lockers})
+def index_view(request):
+    return render(request, 'lockers/index.html')
 
 @login_required
 def notifications_view(request):
