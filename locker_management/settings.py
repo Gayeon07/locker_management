76853a62717b6ad2c57b8dacf8195646e203425f
Gayeon07 <lgy3131@naver.com>
2024-05-26 21:14:38 +0900

    add db

--- locker_management/settings.py
+++ locker_management/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'locker_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'locketing',
+        'USER' : 'root',
+        'PASSWORD' : 'kk61550109*', # 설정한 비밀번호로 적어주면 된다.
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
